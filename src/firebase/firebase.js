dc10cb3341d59539fadffdb5bd84e0b57fc609ef
50Start <74140514+50start@users.noreply.github.com>
2021-08-12 17:28:22 +0900

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAEQe4TX0v8xVLktnE6hUzK6evlWaAtmbo",
-  authDomain: "chakra-firebase.firebaseapp.com",
-  projectId: "chakra-firebase",
-  storageBucket: "chakra-firebase.appspot.com",
-  messagingSenderId: "844786002553",
-  appId: "1:844786002553:web:8e65e1cb1e98f2512882a8",
-  measurementId: "G-9MM1GRF434",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 firebase.initializeApp(firebaseConfig);
