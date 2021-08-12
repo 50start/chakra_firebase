6020a9a4f9b0714ab4d1d978763b94cdadb01067
50start <seriebig18@outlook.jp>
2021-08-12 17:26:33 +0900

    second commit

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
 import { Divider, Flex, Heading, Box, Input, Stack } from "@chakra-ui/react";
 import { memo } from "react";
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const Login = memo(() => {
+  const [userId, setUserId] = useState("");
+
+  const onChangeuserId = (e) => {
+    setUserId(e.target.value);
+  };
+
   return (
     <Flex align="center" justify="center" height="80vh">
       <Box bg="white" w="xs" p={5} borderRadius="xl" shadow="2xl">
@@ -11,7 +18,12 @@ export const Login = memo(() => {
         </Heading>
         <Divider my={5} />
         <Stack spacing={3} py={2} px={2}>
-          <Input placeholder="ユーザーID" />
+          <Input
+            placeholder="ユーザーID"
+            type="text"
+            value={userId}
+            onChange={onChangeuserId}
+          />
           <PrimaryButton>ログイン</PrimaryButton>
         </Stack>
       </Box>
